3c1a05560a2071832dd2a82f47cab13974f92390
Jacob Weaver <jweaver60@gmail.com>
2014-07-26 22:04:35 -0400

    MAILER WORKING

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,10 +5,11 @@ class WelcomeController < ApplicationController
   end
 
   def create
-    @contact = ContactForm.new(params[:contact])
+    @contact = ContactForm.new(params[:contact_form])
     @contact.request = request
     if @contact.deliver
       flash.now[:success] = 'Thank you for contacting us. We will be in touch soon.'
+      render :index
     else
       flash.now[:alert] = 'Cannot send message.'
       render :index
