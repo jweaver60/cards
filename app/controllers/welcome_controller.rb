49ff52a41524a53a61238d8711fb287eae4a35cd
Jacob Weaver <jweaver60@gmail.com>
2014-07-28 22:21:22 -0400

    fixed again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,10 +9,10 @@ class WelcomeController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:success] = 'Thank you for contacting us. We will get in touch soon.'
-      redirect_to :index
+      redirect_to "index"
     else
       flash.now[:alert] = 'Cannot send message.'
-      redirect_to :index
+      redirect_to "index"
     end
   end
 
