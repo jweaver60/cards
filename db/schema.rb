23baca54cd5c32d596df149ca1a3717d07133dc8
Jacob Weaver <jweaver60@gmail.com>
2014-09-14 15:21:27 -0400

    added products model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903232852) do
+ActiveRecord::Schema.define(version: 20140905221638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: true do |t|
     t.string   "name"
-    t.float    "price"
-    t.float    "cost"
     t.text     "description"
+    t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
